fc65ddd36ba1d841e1f8de8d3d33d96ee12aba4a
Udaykiran <officialudaykiran@gmail.com>
2022-12-21 07:49:10 +0530

    queue

--- 05-Queue/02-Queue-With-CircularArray.cpp
+++ 05-Queue/02-Queue-With-CircularArray.cpp
@@ -1,1 +1,47 @@
 // 02-Queue-With-CircularArray
+class Queue{
+	int size;
+	int front;
+	int rear;
+	int capacity;
+	int *arr;
+	// constructor 
+	Queue(int cap){
+		capacity = cap;
+		size = 0;
+		front = -1;
+		rear = -1; 
+		arr = new int[cap];
+	}
+	// is Empty 
+	bool isEmpty(){
+		if(size == 0){
+			return true;
+		}
+		return false;
+	}
+	// is Full 
+	bool isFull(){
+		if(size == capacity){
+			return true;
+		}
+		return false; 
+	}
+	// Enqeuue
+	void enqueue(int x){
+		if(isFull()) return ;
+		rear = (rear+1)%capacity;
+		arr[rear] = x;
+		size++;
+	}
+	// Dequeue
+	int dequeue(){
+		if(isEmpty()){
+			return -1;
+		}
+		int d = arr[front];
+		front = (front+1)%capacity;
+		size--;
+		return d; 
+	}
+};
--- README.md
+++ README.md
@@ -3,12 +3,5 @@
 ##### Topics Left :- 
 
 ```cpp 
-Queue 
-1. Queue With Array 
-2. Circular Queue with Array
 
-BST :- 
-1. Insert 
-2. Delete 
 ```
-
